d91a46d92ff1ff128016f57c1d4950109cdb471e
Konstantin1521 <gorbachevskiy.konstantin@gmail.com>
2025-07-20 18:14:19 +0300

    create new templates update urls and view Update models

--- todo_manager/todo_list/models.py
+++ todo_manager/todo_list/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class ToDoItem(models.Model):
     class Meta:
+        ordering = ("id",)
         verbose_name = "ToDo name"
 
     title = models.CharField(max_length=70)
--- todo_manager/todo_list/views.py
+++ todo_manager/todo_list/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+
+from  . models import ToDoItem
+
+class ToDoListIndexView(ListView):
+    template_name = "todo_list/index.html"
+    queryset = ToDoItem.objects.all()[:3]
+
+
+class ToDoListView(ListView):
+    # template_name = "todo_list/index.html"
+    model = ToDoItem
+
+class ToDoListDoneView(ListView):
+    queryset = ToDoItem.objects.filter(done=True).all()
+
+
+class ToDoDetailView(DetailView):
+    model = ToDoItem
 
 # Create your views here.
+# def index_view(request: HttpRequest) -> HttpResponse:
+#     todo_items = ToDoItem.objects.all()
+#     return  render(
+#         request,
+#         template_name="todo_list/index.html",
+#         context={"todo_items": todo_items[:3]}
+#     )
